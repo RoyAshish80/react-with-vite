29ed704acc8f5419080385c5fdeedfeb902e6235
Ashish Roy <58362174+RoyAshish80@users.noreply.github.com>
2024-10-30 00:09:53 +0530

    Show and Hide alerts
    Show and Hide alerts via button

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
+import Alert from "./components/alert";
 import Button from "./components/Button";
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
   return (
     <div>
-      <Button color="primary" onClick={() => console.log("Clicked!")}>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>Roy's Alert.</Alert>
+      )}
+      <Button color="primary" onClick={() => setAlertVisibility(true)}>
         Roy's Button
       </Button>
     </div>
